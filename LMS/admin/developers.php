9e660a203578524123f44471b3ebfc9d7932b404
Vindula Rathnayake <72916960+VindulaNR@users.noreply.github.com>
2021-10-31 12:01:34 +0530

    final php

---
+++ LMS/admin/developers.php
@@ -0,0 +1,57 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Back</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="admin/images/subhash.jpg" class="img-circle">
+										<hr>
+										<p>Name: Subhash Rathnayaka</p>
+										<p>Address: SriLanka</p>
+										<p>Email: e16320@eng.pdn.ac.lk</p>
+										<p>Position: Programmer</p>
+										</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/vindula.jpg" class="img-circle">
+								<hr>
+										<p>Name: Vindula Rathnayake</p>
+
+										<p>Address: SriLanka</p>
+										<p>Email: e16319@eng.pdn.ac.lk</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/naduni.jpg" class="img-circle">
+								<hr>
+												<p>Name: Naduni Hansika</p>
+										<p>Address: SriLanka</p>
+										<p>Email: e16126@eng.pdn.ac.lk</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
